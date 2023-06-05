b901ceb4cb2565e54e9a48beb6134de23cb04609
RFozzy688 <pokerfozzy@gmail.com>
2023-06-05 20:10:22 +0300

    init project

---
+++ Task_1/Form1.cs
@@ -0,0 +1,12 @@
+namespace Task_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Text = "Создание кнопки";
+        }
+    }
+}
