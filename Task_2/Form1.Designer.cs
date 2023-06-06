2e40703f7fd0d542267cb7897aa86a7b94d9cb3f
RFozzy688 <pokerfozzy@gmail.com>
2023-06-06 11:53:21 +0300

    running button

--- Task_2/Form1.Designer.cs
+++ Task_2/Form1.Designer.cs
@@ -28,18 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(151, 107);
+            button1.Name = "button1";
+            button1.Size = new Size(119, 35);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.MouseMove += button1_MouseMove;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Убегающая кнопка";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
--- Task_2/Form1.cs
+++ Task_2/Form1.cs
@@ -6,5 +6,18 @@ namespace Task_2
         {
             InitializeComponent();
         }
+        private void RelocationButton()
+        {
+            Random random = new Random();
+
+            int x = random.Next(0, ClientSize.Width - button1.Width);
+            int y = random.Next(0, ClientSize.Height - button1.Height);
+
+            button1.Location = new Point(x, y);
+        }
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            RelocationButton();
+        }
     }
 }
