fac219eb4d3632ff8bd72196bdf277e63040ff75
RFozzy688 <pokerfozzy@gmail.com>
2023-06-06 12:23:18 +0300

    start project

---
+++ Task_3/Form1.cs
@@ -0,0 +1,13 @@
+//3) Приложение показывает, сколько времени в секундах осталось до НГ/ДР/окончания 
+//    предмета WinForms (19.08.2023 12:00)
+
+namespace Task_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
